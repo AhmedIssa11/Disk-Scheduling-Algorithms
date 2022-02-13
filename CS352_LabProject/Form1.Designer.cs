a62a45d3c12d06c50ebfb49904fc3a940cf227c5
Ahmed Issa <57608414+AhmedIssa11@users.noreply.github.com>
2022-02-13 13:46:16 +0200

    Add project files.

---
+++ CS352_LabProject/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CS352_LabProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHead = new System.Windows.Forms.TextBox();
+            this.textBoxQueue = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxDisk = new System.Windows.Forms.ComboBox();
+            this.comboBoxDir = new System.Windows.Forms.ComboBox();
+            this.lbDir = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbStart = new System.Windows.Forms.NumericUpDown();
+            this.tbEnd = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Disk Scheduling";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Head";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Queue";
+            // 
+            // textBoxHead
+            // 
+            this.textBoxHead.Location = new System.Drawing.Point(145, 92);
+            this.textBoxHead.Name = "textBoxHead";
+            this.textBoxHead.Size = new System.Drawing.Size(267, 22);
+            this.textBoxHead.TabIndex = 4;
+            // 
+            // textBoxQueue
+            // 
+            this.textBoxQueue.Location = new System.Drawing.Point(145, 58);
+            this.textBoxQueue.Name = "textBoxQueue";
+            this.textBoxQueue.Size = new System.Drawing.Size(267, 22);
+            this.textBoxQueue.TabIndex = 5;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(33, 199);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HeadMovement:";
+            // 
+            // comboBoxDisk
+            // 
+            this.comboBoxDisk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDisk.FormattingEnabled = true;
+            this.comboBoxDisk.Items.AddRange(new object[] {
+            "FCFS - First Come First Serve",
+            "SSTF - Shortest Seek Time First",
+            "SCAN - Elevator",
+            "CSCAN - Circular SCAN ",
+            "LOOK ",
+            "CLOOK  "});
+            this.comboBoxDisk.Location = new System.Drawing.Point(145, 25);
+            this.comboBoxDisk.Name = "comboBoxDisk";
+            this.comboBoxDisk.Size = new System.Drawing.Size(267, 24);
+            this.comboBoxDisk.TabIndex = 8;
+            this.comboBoxDisk.TextChanged += new System.EventHandler(this.comboBoxDisk_TextChanged);
+            // 
+            // comboBoxDir
+            // 
+            this.comboBoxDir.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDir.FormattingEnabled = true;
+            this.comboBoxDir.Items.AddRange(new object[] {
+            "moving towards larger cylinder",
+            "moving towards smaller cylinder"});
+            this.comboBoxDir.Location = new System.Drawing.Point(145, 160);
+            this.comboBoxDir.Name = "comboBoxDir";
+            this.comboBoxDir.Size = new System.Drawing.Size(267, 24);
+            this.comboBoxDir.TabIndex = 10;
+            this.comboBoxDir.Visible = false;
+            this.comboBoxDir.TextChanged += new System.EventHandler(this.comboBoxDir_TextChanged);
+            // 
+            // lbDir
+            // 
+            this.lbDir.AutoSize = true;
+            this.lbDir.Location = new System.Drawing.Point(30, 163);
+            this.lbDir.Name = "lbDir";
+            this.lbDir.Size = new System.Drawing.Size(64, 17);
+            this.lbDir.TabIndex = 9;
+            this.lbDir.Text = "Direction";
+            this.lbDir.Visible = false;
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(33, 241);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.MarkerBorderColor = System.Drawing.Color.Blue;
+            series6.MarkerColor = System.Drawing.Color.Blue;
+            series6.MarkerSize = 10;
+            series6.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series6.Name = "Series1";
+            series6.XAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            series6.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series6.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(379, 297);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ForeColor = System.Drawing.Color.Blue;
+            this.lbResult.Location = new System.Drawing.Point(259, 197);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 20);
+            this.lbResult.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Cylinder";
+            // 
+            // tbStart
+            // 
+            this.tbStart.Location = new System.Drawing.Point(187, 125);
+            this.tbStart.Name = "tbStart";
+            this.tbStart.Size = new System.Drawing.Size(89, 22);
+            this.tbStart.TabIndex = 14;
+            // 
+            // tbEnd
+            // 
+            this.tbEnd.Location = new System.Drawing.Point(313, 125);
+            this.tbEnd.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.tbEnd.Name = "tbEnd";
+            this.tbEnd.Size = new System.Drawing.Size(99, 22);
+            this.tbEnd.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(282, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "To";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(142, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "From";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 560);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbEnd);
+            this.Controls.Add(this.tbStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBoxDir);
+            this.Controls.Add(this.lbDir);
+            this.Controls.Add(this.comboBoxDisk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.textBoxQueue);
+            this.Controls.Add(this.textBoxHead);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Disk Scheduling";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHead;
+        private System.Windows.Forms.TextBox textBoxQueue;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxDisk;
+        private System.Windows.Forms.ComboBox comboBoxDir;
+        private System.Windows.Forms.Label lbDir;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown tbStart;
+        private System.Windows.Forms.NumericUpDown tbEnd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CS352_LabProject/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CS352_LabProject
+{
+    public partial class Form1 : Form
+    {
+        int index, head, direction, totalMov, start, end;
+        bool flagAllDone = false;
+
+        List<int> queue = new List<int>();
+        List<int> movements = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxDisk_TextChanged(object sender, EventArgs e)
+        {
+            string diskAlgo = comboBoxDisk.Text;
+            index = comboBoxDisk.FindString(diskAlgo);
+
+            /* Display the Direction ComboBox */
+            if(index > 1)
+            {
+                lbDir.Visible = true;
+                comboBoxDir.Visible = true;
+            }
+            else
+            {
+                lbDir.Visible = false;
+                comboBoxDir.Visible = false;
+            }
+
+        }
+
+        private void comboBoxDir_TextChanged(object sender, EventArgs e)
+        {
+            /* Larger => 0 
+               Smaller => 1
+            */
+            string algoDir = comboBoxDir.Text;
+            direction = comboBoxDir.FindString(algoDir);
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            /* Check if any textbox is empty */
+            flagAllDone = false;
+            totalMov = 0;
+            movements.Clear();
+
+            if (comboBoxDisk.Text != "" &&
+                textBoxQueue.Text != "" &&
+                textBoxHead.Text != "" &&
+                tbStart.Text != "" &&
+                tbEnd.Text != "")
+            {
+                flagAllDone = true;
+            }
+
+            if (index > 1 && comboBoxDir.Text == "")
+            {
+                flagAllDone = false;
+            }
+
+            /* start processing */
+            if (flagAllDone)
+            {
+                /* get textBoxs values */
+                head = Int32.Parse(textBoxHead.Text);
+                start = Int32.Parse(tbStart.Text);
+                end = Int32.Parse(tbEnd.Text);
+                string diskQueue = textBoxQueue.Text;
+                queue = diskQueue.Split(' ').Select(Int32.Parse).ToList();
+
+                if (index == 0) { FCFS(); }
+                if (index == 1) { SSTF(); }
+                if (index == 2) { SCAN(); }
+                if (index == 3) { CSCAN(); }
+                if (index == 4) { LOOK(); }
+                if (index == 5) { CLOOK(); }
+
+                lbResult.Text = totalMov + "";
+
+                plot();
+            } 
+            else
+            {
+                MessageBox.Show("The field cannot be empty. Please enter a value");
+            }
+        }
+
+        /* IMPORTANT NOTE 
+         * u only can use the following global vars (queue - head - direction - totalMov)
+         * u can't edit any thing outside your function
+         */
+        private void FCFS()
+        {
+            int d, cur_track, headTmp = head;
+            movements.Add(headTmp);
+            for (int i = 0; i < queue.Count(); i++)
+            {
+                cur_track = queue[i];
+                d = Math.Abs(cur_track - headTmp);
+                totalMov += d;
+                headTmp = cur_track;
+                movements.Add(headTmp);
+            }
+        }
+
+        private void SSTF()
+        {
+            int minVal, distance, pos = 0, headTmp = head;
+            movements.Add(head);
+
+            for (int i = 0; i < queue.Count(); i++)
+            {
+                minVal = 999;
+                
+                for (int j = 0; j < queue.Count(); j++)
+                {
+                    if (queue[j] != -1)
+                    {
+                        distance = Math.Abs(headTmp - queue[j]);
+                        if (minVal >= distance)
+                        {
+                            minVal = distance;
+                            pos = j;
+                        }
+                    }
+                }
+
+                totalMov += Math.Abs(headTmp - queue[pos]);
+                headTmp = queue[pos];
+                queue[pos] = -1;
+                movements.Add(headTmp);
+            }
+        }
+
+        private void SCAN()
+        {
+            List<int> queueTmp = queue.ToList();
+            queueTmp.Add(head);
+            queueTmp.Add(start);
+            queueTmp.Add(end);
+            queueTmp.Sort();
+
+            int pos = queueTmp.IndexOf(head);
+
+            if (direction == 0)
+            {
+                
+                for (int i = pos; i < queueTmp.Count(); i++)
+                {
+                    movements.Add(queueTmp[i]);
+                }
+                for (int i = pos - 1; i >= 1; i--)
+                {
+                    movements.Add(queueTmp[i]);
+                }
+
+                totalMov = Math.Abs(head - queueTmp[queueTmp.Count()-1]) + Math.Abs(queueTmp[queueTmp.Count()-1] - queueTmp[1]);
+            }
+            else
+            {
+                for (int i = pos; i >= 0; i--)
+                {
+                    movements.Add(queueTmp[i]);
+                }
+
+                for (int i = pos + 1; i < queueTmp.Count() - 1; i++)
+                {
+                    movements.Add(queueTmp[i]);
+                }
+                totalMov = Math.Abs(head - queueTmp[0]) + Math.Abs(queueTmp[0] - queueTmp[queueTmp.Count() - 2]);
+            }
+        }
+
+        private void CSCAN()
+        {
+
+            if (direction == 0)
+            {
+                int seek_count = 0;
+                int distance, cur_track;
+
+                List<int> left = new List<int>();
+                List<int> right = new List<int>();
+                List<int> seek_sequence = new List<int>();
+                left.Add(0);
+                right.Add(end);
+
+                for (int i = 0; i < queue.Count; i++)
+                {
+                    if (queue[i] < head)
+                        left.Add(queue[i]);
+                    if (queue[i] > head)
+                        right.Add(queue[i]);
+                }
+                left.Sort();
+                right.Sort();
+                for (int i = 0; i < right.Count; i++)
+                {
+                    cur_track = right[i];
+                    seek_sequence.Add(cur_track);
+                    distance = Math.Abs(cur_track - head);
+                    seek_count += distance;
+                    head = cur_track;
+                }
+
+                head = 0;
+                seek_count += (end);
+
+
+                for (int i = 0; i < left.Count; i++)
+                {
+                    cur_track = left[i];
+                    seek_sequence.Add(cur_track);
+                    distance = Math.Abs(cur_track - head);
+                    seek_count += distance;
+                    head = cur_track;
+                }
+
+                totalMov = seek_count;
+                for (int i = 0; i < seek_sequence.Count; i++)
+                {
+                    movements.Add(seek_sequence[i]);
+                }
+            }
+            else
+            {
+                int seek_coun = 0;
+                int distanc, curent_track;
+
+                List<int> left = new List<int>();
+                List<int> right = new List<int>();
+                List<int> seek_sequence = new List<int>();
+                left.Add(0);
+                right.Add(end);
+                for (int i = 0; i < queue.Count; i++)
+                {
+                    if (queue[i] < head)
+                        left.Add(queue[i]);
+                    if (queue[i] > head)
+                        right.Add(queue[i]);
+                }
+
+                left.Sort();
+                right.Sort();
+
+                for (int i = 0; i < left.Count; i++)
+                {
+                    curent_track = left[i];
+                    seek_sequence.Add(curent_track);
+                    distanc = Math.Abs(curent_track - head);
+                    seek_coun += distanc;
+                    head = curent_track;
+                }
+                head = end;
+                seek_coun += head;
+
+
+                for (int i = right.Count - 1; i > 0; i--)
+                {
+                    curent_track = right[i];
+                    seek_sequence.Add(curent_track);
+                    distanc = Math.Abs(curent_track - head);
+                    seek_coun += distanc;
+                    head = curent_track;
+                }
+                totalMov = seek_coun;
+                for (int i = 0; i < seek_sequence.Count; i++)
+                {
+                    movements.Add(seek_sequence[i]);
+                }
+            }
+        }
+
+
+        private void LOOK()
+        {
+            int distance, cur_track, headTmp = head;
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            for (int i = 0; i < queue.Count; i++)
+            {
+                if (queue[i] < head)
+                    left.Add(queue[i]);
+                if (queue[i] > head)
+                    right.Add(queue[i]);
+            }
+            left.Sort();
+            right.Sort();
+            movements.Add(headTmp);
+            int run = 2;
+            while (run-- > 0)
+            {
+                if (direction == 1)
+                {
+                    for (int i = left.Count - 1; i >= 0; i--)
+                    {
+                        cur_track = left[i];
+                        movements.Add(cur_track);
+                        distance = Math.Abs(cur_track - headTmp);
+                        totalMov += distance;
+                        headTmp = cur_track;
+                    }
+                    direction = 0;
+                }
+                else if (direction == 0)
+                {
+                    for (int i = 0; i < right.Count; i++)
+                    {
+                        cur_track = right[i];
+                        movements.Add(cur_track);
+                        distance = Math.Abs(cur_track - headTmp);
+                        totalMov += distance;
+                        headTmp = cur_track;
+                    }
+                    direction = 1;
+                }
+            }
+        }
+    private void CLOOK()
+        {
+            int distance, cur_track, headClook = head;
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            for (int i = 0; i < queue.Count; i++)
+            {
+                if (queue[i] < head)
+                    left.Add(queue[i]);
+                if (queue[i] > head)
+                    right.Add(queue[i]);
+            }
+            left.Sort();
+            right.Sort();
+            movements.Add(headClook);
+            int loop = 2;
+            while (loop-- > 0)
+            {
+                if (direction == 1)
+                {
+                    for (int i = 0; i < left.Count; i++)
+                    {
+                        cur_track = left[i];
+                        movements.Add(cur_track);
+                        distance = Math.Abs(cur_track - headClook);
+                        totalMov += distance;
+                        headClook = cur_track;
+                    }
+                    direction = 0;
+                }
+                else if (direction == 0)
+                {
+                    for (int i = 0; i < right.Count; i++)
+                    {
+                        cur_track = right[i];
+                        movements.Add(cur_track);
+                        distance = Math.Abs(cur_track - headClook);
+                        totalMov += distance;
+                        headClook = cur_track;
+
+                    }
+                    direction = 1;
+                }
+            }
+        }
+
+        private void plot()
+        {
+            chart1.Series["Series1"].Points.Clear();
+            List<int> numbers = new List<int>();
+            int j = movements.Count();
+            for(int i=0;i<movements.Count();i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(movements[i],j);
+                j--;
+            }
+        }
+    }
+}
